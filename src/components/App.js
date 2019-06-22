037586e9114f383eba20b37cece854f5fc62aef4
Pablo Hernandez <prh90@yahoo.com>
2019-06-21 22:48:21 -0700

    Adds fix for state error

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,27 @@ require('dotenv').config();
 
 
 class App extends React.Component {
-  onSearchSubmit(term){
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID '+ process.env.REACT_APP_PICS_API
       }
     });
+
+    this.setState({ images: response.data.results });
   }
 
 
   render(){
-    return <div className="ui container" style={{marginTop: '15px'}}> <SearchBar onSubmit={this.onSearchSubmit}/> </div>;
+    return (
+      <div className="ui container" style={{marginTop: '15px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
+      </div>
+    );
   }
 }
 
