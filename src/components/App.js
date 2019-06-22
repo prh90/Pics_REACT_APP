714838e001d5001a489ec7bba7f837f5e1688bd4
Pablo Hernandez <prh90@yahoo.com>
2019-06-21 22:58:13 -0700

    Adds new file for cleaner code for unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 require('dotenv').config();
 
@@ -8,11 +8,8 @@ class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+    const response = await unsplash.get('/search/photos', {
       params: { query: term },
-      headers: {
-        Authorization: 'Client-ID '+ process.env.REACT_APP_PICS_API
-      }
     });
 
     this.setState({ images: response.data.results });
